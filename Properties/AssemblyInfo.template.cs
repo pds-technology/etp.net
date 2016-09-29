45d71d3ee921c5b814b598f3a73e50ebb06168ca
William S. McKenzie <wsmckenz@home.cartewright.com>
2016-09-29 13:06:50 -0500

    Added AssemblyInfo template

---
+++ Properties/AssemblyInfo.template.cs
@@ -0,0 +1,37 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ETP.NET")]
+[assembly: AssemblyDescription("Message Serialization for Energistics Transfer Protocol")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Energistics")]
+[assembly: AssemblyProduct("EtpClient")]
+[assembly: AssemblyCopyright("Copyright © 2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("8624a10e-221e-4dc7-9917-8a8d6563c4c8")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("__VERSION__.__BUILD__")]
+[assembly: AssemblyFileVersion("__VERSION__.__BUILD__")]
+[assembly: AssemblyMetadata("Git.Sha", "__GITHASH__")]
