0c3e59fcc1ffdeb79711a3e258a072bd8cec08cb
Bobby Diaz <bobby.diaz@pds.nl>
2018-08-13 14:09:38 -0500

    Add interfaces for structures common across multiple ETP versions, e.g. MessageHeader, Acknowledge, ProtocolException, etc.

---
+++ src/ETP.Messages/v11/Extensions.cs
@@ -0,0 +1,61 @@
+﻿//----------------------------------------------------------------------- 
+// ETP DevKit, 1.2
+//
+// Copyright 2018 Energistics
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//   
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//-----------------------------------------------------------------------
+
+using System.Collections;
+using Energistics.Etp.Common.Datatypes;
+using Energistics.Etp.Common.Datatypes.Object;
+using Energistics.Etp.Common.Protocol.Core;
+
+namespace Energistics.Etp.v11
+{
+    namespace Datatypes
+    {
+        namespace Object
+        {
+            public partial class DataObject : IDataObject
+            {
+                IResource IDataObject.Resource
+                {
+                    get { return Resource; }
+                    set { Resource = (Resource) value; }
+                }
+            }
+
+            public partial class Resource : IResource { }
+        }
+
+        public partial class DataValue : IDataValue { }
+
+        public partial class MessageHeader : IMessageHeader { }
+
+        public partial class SupportedProtocol : ISupportedProtocol
+        {
+            IDictionary ISupportedProtocol.ProtocolCapabilities => ProtocolCapabilities as IDictionary;
+        }
+    }
+
+    namespace Protocol
+    {
+        namespace Core
+        {
+            public partial class Acknowledge : IAcknowledge { }
+
+            public partial class ProtocolException : IProtocolException { }
+        }
+    }
+}
---
+++ src/ETP.Messages/v12/Protocols.cs
@@ -0,0 +1,44 @@
+//----------------------------------------------------------------------- 
+// ETP DevKit, 1.2
+//
+// Copyright 2018 Energistics
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//   
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//-----------------------------------------------------------------------
+
+namespace Energistics.Etp.v12
+{
+	public enum Protocols
+	{
+		Core = 0,
+		ChannelStreaming,
+		ChannelDataFrame,
+		Discovery,
+		Store,
+		StoreNotification,
+		GrowingObject,
+		GrowingObjectNotification,
+		WitsmlSoap,
+		DataArray,
+		DataArrayNotification,
+		ChannelStreamingQuery,
+		ChannelDataFrameQuery,
+		DiscoveryQuery,
+		StoreQuery,
+		StoreNotificationQuery,
+		GrowingObjectQuery,
+		GrowingObjectNotificationQuery,
+		Transaction,
+		DirectedDiscovery = 30
+	}
+}
